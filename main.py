cfaf6738fd17e7cf04377c023a465850a159fd01
Adithya-Gonepally <adithya.gonepally@gmail.com>
2025-05-07 00:01:40 +0530

    main.py

--- main.py
+++ main.py
@@ -211,4 +211,5 @@ def manage_user():
         conn.close()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=False, host='0.0.0.0', port=port)
